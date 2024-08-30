4ec0be7d5720b473d4197ebca5dda5008a3af249
jumaster69 <129904546+jumaster69@users.noreply.github.com>
2024-08-30 06:17:33 -0500

    2.3

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,259 +1,381 @@
-import Follow from "../models/follows.js"
-import User from "../models/users.js"
+import Publication from "../models/publications.js"
+import fs from "fs";
+import path from "path";
 import { followUserIds } from "../services/followServices.js"
 
 
 // Método de prueba
-export const testFollow = (req, res) => {
+export const testPublication = (req, res) => {
   return res.status(200).send({
-    message: "Mensaje enviado desde el controlador: follow.js"
+    message: "Mensaje enviado desde el controlador publication.js"
   });
 }
 
-// Método para guardar un follow (seguir a otro usuario)
-export const saveFollow = async (req, res) =>{
+// Método para hacer una publicación
+export const savePublication = async (req, res) => {
   try {
+
     // Obtener datos del body
-    const { followed_user } = req.body;
+    const params = req.body;
 
-    // Obtener el id del usuario autenticado (login) desde el token
-    const identity = req.user;
-
-    // Verificar si "identity" contiene la información del usuario autenticado
-    if (!identity || !identity.userId){
+    // Verificar que llegue desde el body el parámetro text con su información
+    if (!params.text){
       return res.status(400).send({
         status: "error",
-        message: "No se ha proporcionado el usuario para realizar el following"
+        message: "Debes enviar el texto de la publicación"
       });
     }
 
-    // Verificar si el usuario está intentando seguirse a sí mismo
-    if (identity.userId === followed_user) {
-      return res.status(400).send({
-        status: "error",
-        message: "No puedes seguirte a ti mismo"
-      });
-    }
+    // Crear el objeto del modelo
+    let newPublication = new Publication(params);
 
-    // Verificar si el usuario a seguir existe
-    const followedUser = await User.findById(followed_user);
-    if (!followedUser) {
-      return res.status(404).send({
-        status: "error",
-        message: "El usuario que intentas seguir no existe"
-      });
-    }
+    // Agregar la información del usuario autenticado al objeto de la nueva publicación
+    newPublication.user_id = req.user.userId;
 
-    // Verificar si ya existe un seguimiento con los mismos usuarios
-    const existingFollow = await Follow.findOne({
-      following_user: identity.userId,
-      followed_user: followed_user
-    });
+    // Guardar la nueva publicación en la BD
+    const publicationStored = await newPublication.save();
 
-    if(existingFollow) {
-      return res.status(400).send({
-        status: "error",
-        message: "Ya estás siguiendo a este usuario."
-      });
-    }
-
-    // Crear el objeto con modelo follow
-    const newFollow = new Follow({
-      following_user: identity.userId,
-      followed_user: followed_user
-    });
-
-    // Guardar objeto en la BD
-    const followStored = await newFollow.save();
-
-    // Verificar si se guardó correctamente en la BD
-    if(!followStored) {
+    // Verificar si se guardó la publicación en la BD (si existe publicationStored)
+    if (!publicationStored){
       return res.status(500).send({
         status: "error",
-        message: "No se ha podido seguir al usuario."
+        message: "No se ha guardado la publicación"
       });
     }
 
-    // Obtener el nombre y apellido del usuario seguido
-    const followedUserDetails = await User.findById(followed_user).select('name last_name');
+    // Devolver respuesta exitosa 
+    return res.status(200).send({
+      status: "success",
+      message: "¡Publicación creada con éxito!",
+      publicationStored
+    });
 
-    if (!followedUserDetails) {
+  } catch (error) {
+    console.log("Error al crear la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al crear la publicación"
+    });
+  }
+}
+
+// Método para mostrar la publicación
+export const showPublication = async (req, res) => {
+  try {
+
+    // Obtener el id de la publicación de la url
+    const publicationId = req.params.id;
+
+    // Buscar la publicación por id desde la BD
+    const publicationStored = await Publication.findById(publicationId)
+    .populate('user_id', 'name last_name');
+
+    // Verificar si se encontró la publicación
+    if (!publicationStored) {
+      return res.status(500).send({
+        status: "error",
+        message: "No existe la publicación"
+      });
+    }
+
+    // Devolver respuesta exitosa 
+    return res.status(200).send({
+      status: "success",
+      message: "Publicación encontrada",
+      publication: publicationStored
+    });
+
+  } catch (error) {
+    console.log("Error al mostrar la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al mostrar la publicación"
+    });
+  }
+}
+
+// Método para eliminar una publicación
+export const deletePublication = async (req, res) => {
+  try {
+    // Obtener el id de la publicación que se quiere eliminar
+    const publicationId = req.params.id;
+
+    // Encontrar y eliminar la publicación
+    const publicationDeleted = await Publication.findOneAndDelete({ user_id: req.user.userId, _id: publicationId}).populate('user_id', 'name last_name');
+
+    // Verificar si se encontró y eliminó la publicación
+    if (!publicationDeleted) {
       return res.status(404).send({
         status: "error",
-        message: "Usuario seguido no encontrado"
+        message: "No se ha encontrado o no tienes permiso para eliminar esta publicación"
       });
     }
-    
-    // Combinar datos de follow y followedUser
-    const combinedFollowData = {
-      ...followStored.toObject(),
-      followedUser: {
-        name: followedUserDetails.name,
-        last_name: followedUserDetails.last_name
-      }
+
+    // Devolver respuesta exitosa
+    return res.status(200).send({
+      status: "success",
+      message: "Publicación eliminada con éxito",
+      publication: publicationDeleted
+    });
+
+  } catch (error) {
+    console.log("Error al mostrar la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al eliminar la publicación"
+    });
+  }
+}
+
+// Método para listar publicaciones de un usuario
+export const publicationsUser = async (req, res) => {
+  try {
+    // Obtener el id del usuario
+    const userId = req.params.id;
+
+    // Asignar el número de página
+    let page = req.params.page ? parseInt(req.params.page, 10) : 1;
+
+    // Número de usuarios que queremos mostrar por página
+    let itemsPerPage = req.query.limit ? parseInt(req.query.limit, 10) : 5;
+
+    // Configurar las opciones de la consulta
+    const options = {
+      page: page,
+      limit: itemsPerPage,
+      sort: { created_at: -1 },
+      populate: {
+        path: 'user_id',
+        select: '-password -role -__v -email'
+      },
+      lean: true
     };
 
+    // Buscar las publicaciones del usuario
+    const publications = await Publication.paginate({ user_id: userId }, options);
 
-    // Devolver respuesta
-    return res.status(200).json({
-      status: "success",
-      identity: req.user,
-      follow: combinedFollowData
-    });
-
-  } catch (error) {
-    if (error.code === 11000) { // Error de índice único duplicado
-      return res.status(400).json({
-        status: "error",
-        message: "Ya estás siguiendo a este usuario."
-      });
-    }
-    return res.status(500).send({
-      status: "error",
-      message: "Error al seguir al usuario.",
-    });
-  }
-}
-
-// Método para eliminar un follow (dejar de seguir)
-export const unfollow = async (req, res) => {
-  try {
-    // Obtener el Id del usuario indentificado
-    const userId = req.user.userId;
-
-    // Obtener el Id del usuario que sigo y quiero dejar de seguir
-    const followedId = req.params.id;
-
-    // Búsqueda de las coincidencias de ambos usuarios y elimina
-    const followDeleted = await Follow.findOneAndDelete({
-      following_user: userId, // quien realiza el seguimiento
-      followed_user: followedId // a quien se quiere dejar de seguir
-    });
-
-    // Verificar si se encontró el documento y lo eliminó
-    if (!followDeleted) {
+    if (!publications.docs ||publications.docs.length <= 0 ){
       return res.status(404).send({
         status: "error",
-        message: "No se encontró el seguimiento a eliminar."
+        message: "No hay publicaciones para mostrar"
       });
     }
 
-    // Devolver respuesta
+    // Devolver respuesta exitosa
     return res.status(200).send({
       status: "success",
-      message: "Dejaste de seguir al usuario correctamente."
+      message: "Publicaciones del usuario: ",
+      publications: publications.docs,
+      total: publications.totalDocs,
+      pages: publications.totalPages,
+      page: publications.page,
+      limit_items_ppage: publications.limit
     });
 
   } catch (error) {
+    console.log("Error al mostrar la publicación:", error);
     return res.status(500).send({
       status: "error",
-      message: "Error al dejar de seguir al usuario."
+      message: "Error al listar las publicación"
     });
   }
 }
 
-// Método para listar usuarios que estoy siguiendo
-export const following = async (req, res) => {
+// Método para subir archivos (imagen) a las publicaciones que hacemos
+export const uploadMedia = async (req, res) => {
   try {
-    // Obtener el ID del usuario identificado
-    let userId = req.user && req.user.userId ? req.user.userId : undefined;
+    // Obtener el id de la publicación
+    const publicationId = req.params.id;
 
-    // Comprobar si llega el ID por parámetro en la url (este tiene prioridad)
-    if (req.params.id) userId = req.params.id;
-
-    // Asignar el número de página
-    let page = req.params.page ? parseInt(req.params.page, 10) : 1;
-
-    // Número de usuarios que queremos mostrar por página
-    let itemsPerPage = req.query.limit ? parseInt(req.query.limit, 10) : 5;
-
-    // Configurar las opciones de la consulta
-    const options = {
-      page: page,
-      limit: itemsPerPage,
-      populate: {
-        path: 'followed_user',
-        select: '-password -role -__v -email'
-      },
-      lean: true
+    // Verificar si la publicación existe en la base de datos antes de subir el archivo
+    const publicationExists = await Publication.findById(publicationId);
+    if (!publicationExists) {
+      return res.status(404).send({
+        status: "error",
+        message: "No existe la publicación"
+      });
     }
+
+    // Comprobamos que existe el archivo en el body
+    if (!req.file) {
+      return res.status(404).send({
+        status: "error",
+        message: "La petición no incluye la imagen"
+      });
+    }
+
+    // Obtener el nombre del archivo
+    let image = req.file.originalname;
     
-    // Buscar en la BD los seguidores y popular los datos de los usuarios
-    const follows = await Follow.paginate({ following_user: userId }, options);
+    // Obtener la extensión del archivo
+    const imageSplit = image.split(".");
+    const extension = imageSplit[imageSplit.length -1];
 
-    // Listar los seguidores de un usuario, obtener el array de IDs de los usuarios que sigo
-    let followUsers = await followUserIds(req);
+    // Validar la extensión
+    if (!["png", "jpg", "jpeg", "gif"].includes(extension.toLowerCase())){
+      //Borrar archivo subido
+      const invalidFilePath = req.file.path;
+      fs.unlinkSync(invalidFilePath );
 
-    // Devolver respuesta
+      return res.status(400).send({
+        status: "error",
+        message: "Extensión del archivo es inválida."
+      });
+    }
+
+    // Comprobar tamaño del archivo (pj: máximo 1MB)
+    const fileSize = req.file.size;
+    const maxFileSize = 1 * 1024 * 1024; // 1 MB
+
+    if (fileSize > maxFileSize) {
+      const largeFilePath = req.file.path;
+      fs.unlinkSync(largeFilePath );
+
+      return res.status(400).send({
+        status: "error",
+        message: "El tamaño del archivo excede el límite (máx 1 MB)"
+      });
+    }
+
+    // Verificar si el archivo realmente existe antes de proceder
+    const actualFilePath  = path.resolve("./uploads/publications/", req.file.filename);
+    try {
+      fs.statSync(actualFilePath); 
+    } catch (error) {
+      return res.status(404).send({
+        status: "error",
+        message: "El archivo no existe o hubo un error al verificarlo"
+      });
+    }
+
+    // Si todo es correcto, se procede a guardar en la BD
+    const publicationUpdated = await Publication.findOneAndUpdate(
+      { user_id: req.user.userId, _id: publicationId },
+      { file: req.file.filename },
+      { new: true }
+    );
+
+    if (!publicationUpdated) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error en la subida del archivo"
+      });
+    }
+
+    // Devolver respuesta exitosa
     return res.status(200).send({
       status: "success",
-      message: "Listado de usuarios que estoy siguiendo",
-      follows: follows.docs,
-      total: follows.totalDocs,
-      pages: follows.totalPages,
-      page: follows.page,
-      limit: follows.limit,
-      users_following: followUsers.following,
-      user_follow_me: followUsers.followers
+      message: "Archivo subido con éxito",
+      publication: publicationUpdated,
+      file: req.file
     });
 
   } catch (error) {
     return res.status(500).send({
       status: "error",
-      message: "Error al listar los usuarios que estás siguiendo."
+      message: "Error al subir el archivo a la publicación"
     });
   }
 }
 
-// Método para listar los usuarios que me siguen
-export const followers = async (req, res) => {
+// Método para mostrar el archivo subido a la publicación
+export const showMedia = async (req, res) => {
   try {
-    // Obtener el ID del usuario identificado
-    let userId = req.user && req.user.userId ? req.user.userId : undefined;
+    // Obtener el parámetro del archivo desde la url
+    const file = req.params.file;
 
-    // Comprobar si llega el ID por parámetro en la url (este tiene prioridad)
-    if (req.params.id) userId = req.params.id;
+    // Crear el path real de la imagen
+    const filePath = "./uploads/publications/" + file;
 
-    // Asignar el número de página
-    let page = req.params.page ? parseInt(req.params.page, 10) : 1;
-
-    // Número de usuarios que queremos mostrar por página
-    let itemsPerPage = req.query.limit ? parseInt(req.query.limit, 10) : 5;
-
-    // Configurar las opciones de la consulta
-    const options = {
-      page: page,
-      limit: itemsPerPage,
-      populate: {
-        path: 'following_user',
-        select: '-password -role -__v -email'
-      },
-      lean: true
-    }
-    
-    // Buscar en la BD los seguidores y popular los datos de los usuarios
-    const follows = await Follow.paginate({ followed_user: userId }, options);
-
-    // Listar los seguidores de un usuario, obtener el array de IDs de los usuarios que sigo
-    let followUsers = await followUserIds(req);
-
-    // Devolver respuesta
-    return res.status(200).send({
-      status: "success",
-      message: "Listado de usuarios que me siguen",
-      follows: follows.docs,
-      total: follows.totalDocs,
-      pages: follows.totalPages,
-      page: follows.page,
-      limit: follows.limit,
-      users_following: followUsers.following,
-      user_follow_me: followUsers.followers
+    // Comprobar si existe el archivo
+    fs.stat(filePath, (error, exists) => {
+      if(!exists) {
+        return res.status(404).send({
+          status: "error",
+          message: "No existe la imagen"
+        });
+      }
+      // Si lo encuentra nos devolvueve un archivo
+      return res.sendFile(path.resolve(filePath));
     });
 
   } catch (error) {
     return res.status(500).send({
       status: "error",
-      message: "Error al listar los usuarios que me siguen."
+      message: "Error al mostrar archivo en la publicación"
+    });
+  }
+}
+
+// Método para listar todas las publicaciones de los usuarios que yo sigo (Feed)
+export const feed = async (req, res) => {
+  try {
+    // Asignar el número de página
+    let page = req.params.page ? parseInt(req.params.page, 10) : 1;
+
+    // Número de publicaciones que queremos mostrar por página
+    let itemsPerPage = req.query.limit ? parseInt(req.query.limit, 10) : 5;
+
+    // Verificar que el usuario autenticado existe y tiene un userId
+    if(!req.user || !req.user.userId) {
+      return res.status(404).send({
+        status: "error",
+        message: "Usuario no autenticado"
+      });
+    }
+
+    // Obtener un array de IDs de los usuarios que sigue el usuario autenticado
+    const myFollows = await followUserIds(req);
+
+    // Verificar que la lista de usuarios que sigo no esté vacía
+    if (!myFollows.following || myFollows.following.length === 0){
+      return res.status(404).send({
+        status: "error",
+        message: "No sigues a ningún usuario, no hay publicaciones que mostrar"
+      });
+    }
+
+    // Configurar las options de la consulta
+    const options = {
+      page: page,
+      limit: itemsPerPage,
+      sort: { created_at: -1 },
+      populate: {
+        path: 'user_id',
+        select: '-password -role -__v -email'
+      },
+      lean: true
+    };
+
+    // Consulta a la base de datos con paginate
+    const result = await Publication.paginate(
+      { user_id: { $in: myFollows.following }},
+      options
+    );
+
+    // Verificar si se encontraron publicaciones en la BD
+    if (!result.docs || result.docs.length <= 0) {
+      return res.status(404).send({
+        status: "error",
+        message: "No hay publicaciones para mostrar"
+      });
+    }
+
+    // Devolver respuesta exitosa
+    return res.status(200).send({
+      status: "success",
+      message: "Feed de Publicaciones",
+      publications: result.docs,
+      total: result.totalDocs,
+      pages: result.totalPages,
+      page: result.page,
+      limit: result.limit
+    });
+
+  } catch (error) {
+    return res.status(500).send({
+      status: "error",
+      message: "Error al mostrar las publicaciones en el feed"
     });
   }
 }
